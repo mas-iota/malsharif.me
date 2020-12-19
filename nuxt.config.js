6570f8c34efe5cbbe2a724634eaa14843492946b
Mahmoud Al-Sharif <mahmoud.k.alsharif@gmail.com>
2020-12-19 12:28:42 +0200

    feat(app): removed font prefetching

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,9 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
